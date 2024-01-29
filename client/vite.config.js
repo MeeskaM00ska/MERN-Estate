4579d76a27615225026dd7a4ea0e5159824e733e
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-01-29 15:38:25 +0800

    sign up page funcionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+// 如果你的前端应用尝试访问 /api/user，这个请求会被代理到 http://localhost:8080/api/user。
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
