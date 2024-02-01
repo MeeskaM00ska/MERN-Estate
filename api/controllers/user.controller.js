055c5abe022a743ca7f89aab0a4281f744d25046
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-02-01 14:48:35 +0800

    create update user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,35 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
   res.json({ message: "Hello world!" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true } //new：如果为 true，返回的将是更新后的文档；如果为 false，返回的是原始文档。默认值为 false。
+    );
+
+    const { password, ...rest } = updatedUser._doc; //分离password
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
