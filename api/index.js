f84eb49fcba59e536fb29216f65ea37b467ad19e
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-02-02 13:17:25 +0800

    create listing api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 mongoose
@@ -21,12 +22,19 @@ app.use(express.json());
 // get info from the cookie
 app.use(cookieParser());
 
+// app.use((req, res, next) => {
+//   console.log("Headers:", req.headers);
+//   console.log("Body:", req.body);
+//   next();
+// });
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080...");
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // ?
 app.use((err, req, res, next) => {
