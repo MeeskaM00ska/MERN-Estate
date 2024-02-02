a477f5c1158c686e6024eac8e5035ca3cb92726f
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-02-02 14:06:53 +0800

    complete listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,7 +17,7 @@ import {
   deleteUserSuccess,
   signOutUserStart,
 } from "../redux/user/userSlice";
-import { asyncThunkCreator } from "@reduxjs/toolkit";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const fileRef = useRef(null);
@@ -189,6 +189,12 @@ const Profile = () => {
         >
           {loading ? "loading..." : "update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
