048cd2688505a81825842fe2100ecd3f98ac983c
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-02-12 11:27:51 +0800

    Create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 const Search = () => {
   const navigate = useNavigate();
@@ -210,10 +211,25 @@ const Search = () => {
           </button>
         </form>
       </div>
-      <div>
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold p-3 border-b text-slate-700 mt-5">
           Listing results:
         </h1>
+        <div className="flex flex-wrap p-7 gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found!</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">
+              Loading...
+            </p>
+          )}
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
       </div>
     </div>
   );
